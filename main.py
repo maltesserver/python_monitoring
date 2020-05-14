9387581ad51d123e5ca0970092fa33c8d54fc7c4
Realtox <f.gassan.fg@gmail.com>
2020-05-14 11:37:44 +0200

    Edit the main file and add some functions

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import psutil
+import wmi
+
 cpu = psutil.cpu_percent()
 vram = psutil.virtual_memory()
-dvram = dict(psutil.virtual_memory()._asdict())
 
+c = wmi.WMI()
+print ("INFO")
+systeminfo = c.Win32_ComputerSystem()[0]
+
+Manufacturer = systeminfo.Manufacturer
+Model = systeminfo.Model
+
+print(systeminfo)
+print(Manufacturer)
+print(Model)
+
+print ("CPU")
 print(cpu)
+print ("RAM")
 print(vram)
-print(dvram)
