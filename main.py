ea389f86897f93cf2ef7a76a30cda3e22998cf93
Realtox <f.gassan.fg@gmail.com>
2020-05-14 11:26:26 +0200

    Edit the main file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-x = 3
-if x == 3:
-    # indented four spaces
-    print("x is 3.")
+import psutil
+cpu = psutil.cpu_percent()
+vram = psutil.virtual_memory()
+dvram = dict(psutil.virtual_memory()._asdict())
+
+print(cpu)
+print(vram)
+print(dvram)
