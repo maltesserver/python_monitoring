53359e5c87213b42c400031d4829e698e81fa6ec
Realtox <39673627+Realtox@users.noreply.github.com>
2020-05-14 12:28:55 +0200

    Update main.py
    Edit the main file and add some functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import psutil
 import wmi
+import os
 
 cpu = psutil.cpu_percent()
 vram = psutil.virtual_memory()
@@ -15,7 +16,21 @@ print(systeminfo)
 print(Manufacturer)
 print(Model)
 
+
 print ("CPU")
 print(cpu)
 print ("RAM")
 print(vram)
+
+print ("")
+print ("[------Ping Test------]")
+print ("")
+
+hostname = "google.com" #example
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+  print (hostname, "is up!")
+else:
+  print (hostname, "is down!")
